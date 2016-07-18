3151fa1181876eee0ebb42f22c87c9a8c6c2f355
Dániel Sebestyén <daniel.sebestyen@oncompassmedicine.com>
2016-07-18 14:02:02 +0200

    utils arrayInsert fn

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,3 +1,7 @@
 export function commaStringToArray(input:string):Array<string>{
     return input.split(',').map((chunk:string) => chunk.trim());
 }
+export function arrayInsert(stack: Array<any>, element: any, index: number):Array<any>{
+    stack.splice(index, 0, element);
+    return stack;
+}
